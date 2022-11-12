1198195c703dfb46b63a98b80741c92be467aee4
mdOmarfaruk151 <mdomar28151@gmile.com>
2022-11-13 05:56:00 +0600

    dragon-news-server

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
+const cors = require("cors");
 
 //! for get ride of api load error
 app.use(cors());
 
-//! get data from folder file
+//! get data from folder .json file
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
-app.get('/', (req, res) => {
-  res.send('News API Running');
+app.get("/", (req, res) => {
+  res.send("News API Running");
 });
 
 //! for send data categories
-app.get('/news-categories', (req, res) => {
+app.get("/news-categories", (req, res) => {
   res.send(categories);
+});
+
+//! for send data news by selected category
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const category_news = news.filter((n) => n.category_id === id);
+    res.send(category_news);
+  }
+});
+
+//! for send all data news  
+app.get('/news',(req, res)=>{
+  res.send(news);
+})
+
+
+//! for send data news by selected id
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
 });
 
 app.listen(port, () => {
   console.log(`Dragon News Server running on port `, port);
-})
+});
